ca1ffb4bcecebb929e08c80cd692ddc60c1b50ce
sena-tan <sena.tan.bayraktar@hotmail.com>
2020-03-23 09:08:05 +0200

    Primitives Shapes - ellipse /wface

--- module08/ex823.js
+++ module08/ex823.js
@@ -1,7 +1,18 @@
 function setup() {
-	createCanvas(800,500);
-	background('#31bc33');
+	createCanvas(800,600);
+	background('grey');
 	}
 function draw () {
-    ellipse(width/2,height/2,60);
+    ellipseMode(RADIUS);
+    fill("purple");
+    ellipse(width/2,height/2,200);
+    fill("white");
+    ellipse(320,height/3,50);
+    ellipse(480,height/3,50);
+    ellipse(width/2,400,70);
+    ellipseMode(CORNERS);
+    fill("black");
+    ellipse(295,height/3,345,250);
+    ellipse(455,height/3,505,250)
+   
   }
