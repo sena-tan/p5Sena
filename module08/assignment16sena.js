4d0df6f147c6f4750f0f414d57348a0b9a104397
sena-tan <sena.tan.bayraktar@hotmail.com>
2020-03-23 19:36:51 +0200

    ass16 /wcolor

--- module08/assignment16sena.js
+++ module08/assignment16sena.js
@@ -4,19 +4,34 @@ function setup() {
     createCanvas(800, 500);
     background(255, 255, 204);
     noStroke();
-    var r = 0; r <=255; r = r + 90;
-    var b = 0; b <= 255; b = b + 60;
-    fill(r, 21, b);
-        for (xPos = 20; xPos < width; xPos = xPos + 40) {
-            for (yPos = 50; yPos < height; yPos = yPos + 100) {
-                ellipseMode(RADIUS);
-                ellipse(xPos, yPos, 10,10);
-            }
+     for (xPos = 20; xPos < width; xPos = xPos + 40) {
+        for (yPos = 50; yPos < height; yPos = yPos + 100) {
+            ellipseMode(RADIUS);
+            fill(xPos/4, 20, yPos/2);
+            ellipse(xPos, yPos, 10,10);
         }
+    }
+    // for ( r = 0; r <= 255; r++ )
+    // {
+    //   for ( g = 0; g <= 255; g++ )
+    //   {
+    //     for ( b = 0; b <= 255; b++)
+    //     {
+    //         fill(r, g, b);
+    //         for (xPos = 20; xPos < width; xPos = xPos + 40) {
+    //             for (yPos = 50; yPos < height; yPos = yPos + 100) {
+    //                 ellipseMode(RADIUS);
+    //                 ellipse(xPos, yPos, 10,10);
+    //             }
+    //         }
+    //     }
+    //   }
+    // }
+    
 }
 function draw()  {
     noStroke();
-    fill(204, 255, 102)
+    fill(204, 235, 102)
     for (xPos = 20; xPos < width; xPos = xPos + 40) {
         ellipseMode(CENTER);
         ellipse(xPos, height/2, 10,10);}
