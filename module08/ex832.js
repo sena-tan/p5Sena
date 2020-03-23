2825020e2886e1ac7abda93578d8adfccf5a8439
sena-tan <sena.tan.bayraktar@hotmail.com>
2020-03-23 11:02:35 +0200

    Color Rules

--- module08/ex832.js
+++ module08/ex832.js
@@ -1,17 +1,17 @@
 function setup() {
 	createCanvas(800,600);
-	background('purple');
+	background('grey');
 	}
 function draw () {
     ellipseMode(RADIUS);
-    // fill("purple");
+    fill("purple");
     ellipse(width/2,height/2,200);
-    // fill("white");
+    fill("white");
     ellipse(320,height/3,50);
     ellipse(480,height/3,50);
     ellipse(width/2,400,70);
     ellipseMode(CORNERS);
-    // fill("black");
+    fill("black");
     ellipse(295,height/3,345,250);
     ellipse(455,height/3,505,250)
    
