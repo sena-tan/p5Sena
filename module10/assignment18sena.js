cf834ce0a49ca581709f51c6194cdd943c0fed6f
sena-tan <sena.tan.bayraktar@hotmail.com>
2020-03-29 06:31:24 +0300

    assignment18final

--- module10/assignment18sena.js
+++ module10/assignment18sena.js
@@ -1,27 +1,27 @@
 let camVideo;
 function setup(){
-  createCanvas(600,800);
-  // make the connection to the webcam
+  createCanvas(600,600);
   camVideo = createCapture('VIDEO');
-  //set background color
   background(244,98,44);
   myButton = createButton('take pics');
-  // when button is pressed call the PausePlay function
   myButton.mousePressed(ScreenShot);
 }
+
+
 function draw(){
-    camVideo.loadPixels();
-    image(camVideo,0,0);
-    screens.loadPixels();
+  image(camVideo,0,0);
 }
 
 function ScreenShot() {
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-          let myPixel = camVideo.get(x, y);
-          screens.set(x,y, myPixel);
-        }
+  
+  for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        pic=camVideo.get()
+        screens.set(x,y, pic);
       }
-      screens.updatePixels();
-      image(screens, 0, 0);
+    }
+  
+    image(screens, 0, 0);
 }
+
+
